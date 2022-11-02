0fc0a27aa50e5e293d327227467cceae79bd51de
jeonmink <minkmink0092@gmail.com>
2022-11-02 20:49:44 +0700

    ddddd

--- list.php
+++ list.php
@@ -70,7 +70,7 @@ $date_out = isset($_POST['date_out']) ? $_POST['date_out'] : date('Y-m-d', strto
 								<img src="assets/img/<?php echo $cat_arr[$row['category_id']]['cover_img'] ?>" alt="">
 							</div>
 							<div class="col-md-5" height="100%">
-								<h3><b><?php echo '$ ' . number_format($cat_arr[$row['category_id']]['price'], 2) ?></b><span> / วัน</span></h3>
+								<h3><b><?php echo '$ ' . number_format($cat_arr[$row['category_id']]['price'], 2) ?></b><span> / ต่อวัน</span></h3>
 
 								<h4><b>
 										<?php echo $cat_arr[$row['category_id']]['name'] ?>
